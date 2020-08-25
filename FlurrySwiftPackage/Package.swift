2ad7f4da28fc6a1679f69bb72a06c3ba55f7682a
hunterhays <hhays@yahoo-inc.com>
2020-08-24 18:54:50 -0700

    Update Package.swift

--- FlurrySwiftPackage/Package.swift
+++ FlurrySwiftPackage/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FlurrySwiftPackage",
-            targets: ["FlurrySwiftPackage"]),
+            targets: ["FlurrySwiftPackage", "FlurryMessaging.xcframework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
